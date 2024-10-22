435e87347dad498166727ea28a16113f3ba01614
Никита Шидогубов <nikita@mer1d1an.ru>
2024-10-22 09:10:40 +0300

    добавление функций

--- itr/models.py
+++ itr/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
@@ -95,6 +97,26 @@ class Employee(models.Model):
     def __str__(self):
         return f"{self.last_name} {self.first_name[0]}. {self.middle_name[0]}."
 
+    def get_birth_date_with_age(self):
+        today = date.today()
+        age = today.year - self.date_of_birth.year - (
+                    (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
+        return f" (Возраст {age})"
+
     class Meta:
         verbose_name = "Сотрудник"
         verbose_name_plural = "Сотрудники"
+
+
+class Object(models.Model):
+    ooogk = 'ООО ГК "ЛифтСервис"'
+    ooocs = 'ООО "ЦентрСервис"'
+    ooohcs = 'ООО Холдинг "ЦентрСервис"'
+    ooo_firm = [
+        (ooogk, 'ООО ГК "ЛифтСервис"'),
+        (ooocs, 'ООО "ЦентрСервис"'),
+        (ooohcs, 'ООО Холдинг "ЦентрСервис"'),
+    ]
+
+    firm = models.CharField(max_length=30, choices=ooo_firm, verbose_name="Фирма", help_text="Выберите фирму")
+    address = models.CharField(max_length=100, verbose_name="Адрес", help_text="Введите адрес")
